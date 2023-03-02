ca0771cf436ffebab0ab76c3b72f91f530614b16
Kunj1311 <74526794+KUNJ1311@users.noreply.github.com>
2023-03-02 20:18:48 +0530

    fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,7 @@
-import React, { Component } from 'react'
-import * as Components from './components'
+import React from "react";
 
-export default function Login() {
-  const [signin,toggle]= React.useState(true);
-  return (
-    
-    <>  
-    <Components.container>
-     <Components.signupcontainer signinIn={signin}>
-        <Components.Form>
-           <Components.Title>Create Account</Components.Title>
-            <Components.Input type='text' placeholder='Name'/>
-            <Components.Input type='email' placeholder='Email'/>
-            <Components.Input type='password' placeholder='Password'/>
-            <Components.Button>Sign Up</Components.Button>
-         </Components.Form>
-       </Components.signupcontainer>
+const Login = () => {
+	return <div>Login</div>;
+};
 
-       <Components.signincontainer signinIn={signin}>
-        <Components.overlay signIn={signin}>
-          <Components.Title>Welcome back</Components.Title>
-          <Components.paragraph>
-            To keep  connected with us please login with your personal information 
-          </Components.paragraph>
-          <Components.ghostButton onclick={()=> toggle(true)}>
-            SignIn
-        </Components.ghostButton>
-        </Components.overlay>
-       </Components.signincontainer>
-       </Components.container>
-     </>
-
-  )
-}
+export default Login;
