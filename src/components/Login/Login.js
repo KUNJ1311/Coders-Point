de90ea339f20f212c9cc9db56b0c9bef737fcc78
rohit343434 <mr7909626@gmail.com>
2023-03-20 22:44:56 +0530

    okay

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,86 +2,97 @@ import React from "react";
 import "./login.css";
 
 export default function Login() {
+	const container = () => {
+		document.getElementById("container");
+	  };
+	  const signUpButton = () => {
+		container.classList.add("right-panel-active");
+	  };
+	  const signInButton = () => {
+		container.classList.remove("right-panel-active");
+	  };
+	
 	return (
 		<>
-			<section className="sec">
-				<div className="container" id="container">
-					<div className="form-container sign-up-container">
-						<form action="#">
-							<h1 className="h">Create Account </h1>
-							<div className="social-container">
-								<a className="social">
-									<i className="fab fa-facebook-f"></i>
-								</a>
-								<a className="social">
-									<i className="fab fa-google-plus-g"></i>
-								</a>
-								<a className="social">
-									<i className="fab fa-linkedin-in"></i>
-								</a>
-							</div>
-							<span>or use your email for registration</span>
-							<div className="infield">
-								<input type="text" placeholder="Name" />
-								<label></label>
-							</div>
-							<div className="infield">
-								<input type="email" placeholder="Email" name="email" />
-								<label></label>
-							</div>
-							<div className="infield">
-								<input type="password" placeholder="Password" />
-								<label></label>
-							</div>
-							<button>Sign 1Up</button>
-						</form>
-					</div>
-					<div className="form-container sign-in-container">
-						<form action="#">
-							<h1 className="h">Sign in</h1>
-							<div className="social-container">
-								<a className="social">
-									<i className="fab fa-facebook-f"></i>
-								</a>
-								<a className="social">
-									<i className="fab fa-google-plus-g"></i>
-								</a>
-								<a className="social">
-									<i className="fab fa-linkedin-in"></i>
-								</a>
-							</div>
-							<span>or use your account</span>
-							<div className="infield">
-								<input type="email" placeholder="Email" name="email" />
-								<label></label>
-							</div>
-							<div className="infield">
-								<input type="password" placeholder="Password" />
-								<label></label>
-							</div>
-							<a href="#" className="forgot">
-								Forgot your password?
+		
+			<section className="sec"><div className="container" id="container">
+				<div className="form-container sign-up-container">
+					<form action="#">
+						<h1 className="h">Create Account</h1>
+						<div className="social-container">
+							<a className="social">
+								<i className="fab fa-facebook-f"></i>
 							</a>
-							<button>Sign In</button>
-						</form>
-					</div>
-					<div className="overlay-container" id="overlayCon">
-						<div className="overlay">
-							<div className="overlay-panel overlay-left">
-								<h1 className="h">Welcome Back!</h1>
-								<p>To keep connected with us please login with your personal info</p>
-								<button>Sign In</button>
-							</div>
-							<div className="overlay-panel overlay-right">
-								<h1 className="h">Hello, Friend!</h1>
-								<p>Enter your personal details and start journey with us</p>
-								<button>Sign Up</button>
-							</div>
+							<a className="social">
+								<i className="fab fa-google-plus-g"></i>
+							</a>
+							<a className="social">
+								<i className="fab fa-linkedin-in"></i>
+							</a>
 						</div>
-						<button id="overlayBtn"></button>
-					</div>
+						<span>or use your email for registration</span>
+						<div className="infield">
+							<input type="text" placeholder="Name" />
+							<label></label>
+						</div>
+						<div className="infield">
+							<input type="email" placeholder="Email" name="email" />
+							<label></label>
+						</div>
+						<div className="infield">
+							<input type="password" placeholder="Password" />
+							<label></label>
+						</div>
+						<button className="robtn" onClick={()=>console.log("rohit")}>Sign Up</button>
+					</form>
 				</div>
+				<div className="form-container sign-in-container">
+					<form action="#">
+						<h1 className="h">Sign in</h1>
+						<div className="social-container">
+							<a className="social">
+								<i className="fab fa-facebook-f"></i>
+							</a>
+							<a className="social">
+								<i className="fab fa-google-plus-g"></i>
+							</a>
+							<a className="social">
+								<i className="fab fa-linkedin-in"></i>
+							</a>
+						</div>
+						<span>or use your account</span>
+						<div className="infield">
+							<input type="email" placeholder="Email" name="email" />
+							<label></label>
+						</div>
+						<div className="infield">
+							<input type="password" placeholder="Password" />
+							<label></label>
+						</div>
+						<a href="#" className="forgot">
+							Forgot your password?
+						</a>
+						<button className="robtn" onClick={()=>console.log("rohit")}>Sign In</button>
+					</form>
+				</div>
+				<div className="overlay-container" id="overlayCon">
+					<div className="overlay">
+						<div className="overlay-panel overlay-left">
+							<h1 className="h">Welcome Back!</h1>
+							<p className="p">To keep connected with us please login with your personal info</p>
+							<button className="robtn" onClick={()=>console.log("rohit")}>Sign In</button>
+						</div>
+						<div className="overlay-panel overlay-right">
+							<h1 className="h">Hello, Friend!</h1>
+							<p className="p">Enter your personal details and start journey with us</p>
+							<button className="robtn" >Signs Up</button>
+						</div>
+					</div>
+					<button id="overlayBtn" onClick={()=>console.log("rohit")}></button>
+				</div>
+			</div>
 			</section>
+
 
 			{/* <script>
 				const container=document.getelementbyid('container')
@@ -99,6 +110,9 @@ export default function Login() {
 
 			</script>
  */}
+
+
+			
 		</>
 	);
 }
