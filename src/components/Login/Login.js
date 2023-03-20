1f5a6ca4a4cc2e2aa7c43df228973ce1aef42886
Kunj1311 <74526794+KUNJ1311@users.noreply.github.com>
2023-03-20 21:37:58 +0530

    ---

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,88 +2,86 @@ import React from "react";
 import "./login.css";
 
 export default function Login() {
-	
 	return (
 		<>
-		
-			<section className="sec"><div className="container" id="container">
-				<div className="form-container sign-up-container">
-					<form action="#">
-						<h1 className="h">Create Account</h1>
-						<div className="social-container">
-							<a className="social">
-								<i className="fab fa-facebook-f"></i>
-							</a>
-							<a className="social">
-								<i className="fab fa-google-plus-g"></i>
-							</a>
-							<a className="social">
-								<i className="fab fa-linkedin-in"></i>
-							</a>
-						</div>
-						<span>or use your email for registration</span>
-						<div className="infield">
-							<input type="text" placeholder="Name" />
-							<label></label>
-						</div>
-						<div className="infield">
-							<input type="email" placeholder="Email" name="email" />
-							<label></label>
-						</div>
-						<div className="infield">
-							<input type="password" placeholder="Password" />
-							<label></label>
-						</div>
-						<button>Sign 1Up</button>
-					</form>
-				</div>
-				<div className="form-container sign-in-container">
-					<form action="#">
-						<h1 className="h">Sign in</h1>
-						<div className="social-container">
-							<a className="social">
-								<i className="fab fa-facebook-f"></i>
-							</a>
-							<a className="social">
-								<i className="fab fa-google-plus-g"></i>
-							</a>
-							<a className="social">
-								<i className="fab fa-linkedin-in"></i>
-							</a>
-						</div>
-						<span>or use your account</span>
-						<div className="infield">
-							<input type="email" placeholder="Email" name="email" />
-							<label></label>
-						</div>
-						<div className="infield">
-							<input type="password" placeholder="Password" />
-							<label></label>
-						</div>
-						<a href="#" className="forgot">
-							Forgot your password?
-						</a>
-						<button>Sign In</button>
-					</form>
-				</div>
-				<div className="overlay-container" id="overlayCon">
-					<div className="overlay">
-						<div className="overlay-panel overlay-left">
-							<h1 className="h">Welcome Back!</h1>
-							<p>To keep connected with us please login with your personal info</p>
-							<button>Sign In</button>
-						</div>
-						<div className="overlay-panel overlay-right">
-							<h1 className="h">Hello, Friend!</h1>
-							<p>Enter your personal details and start journey with us</p>
-							<button>Sign Up</button>
-						</div>
+			<section className="sec">
+				<div className="container" id="container">
+					<div className="form-container sign-up-container">
+						<form action="#">
+							<h1 className="h">Create Account rohit</h1>
+							<div className="social-container">
+								<a className="social">
+									<i className="fab fa-facebook-f"></i>
+								</a>
+								<a className="social">
+									<i className="fab fa-google-plus-g"></i>
+								</a>
+								<a className="social">
+									<i className="fab fa-linkedin-in"></i>
+								</a>
+							</div>
+							<span>or use your email for registration</span>
+							<div className="infield">
+								<input type="text" placeholder="Name" />
+								<label></label>
+							</div>
+							<div className="infield">
+								<input type="email" placeholder="Email" name="email" />
+								<label></label>
+							</div>
+							<div className="infield">
+								<input type="password" placeholder="Password" />
+								<label></label>
+							</div>
+							<button>Sign 1Up</button>
+						</form>
 					</div>
-					<button id="overlayBtn"></button>
+					<div className="form-container sign-in-container">
+						<form action="#">
+							<h1 className="h">Sign in</h1>
+							<div className="social-container">
+								<a className="social">
+									<i className="fab fa-facebook-f"></i>
+								</a>
+								<a className="social">
+									<i className="fab fa-google-plus-g"></i>
+								</a>
+								<a className="social">
+									<i className="fab fa-linkedin-in"></i>
+								</a>
+							</div>
+							<span>or use your account</span>
+							<div className="infield">
+								<input type="email" placeholder="Email" name="email" />
+								<label></label>
+							</div>
+							<div className="infield">
+								<input type="password" placeholder="Password" />
+								<label></label>
+							</div>
+							<a href="#" className="forgot">
+								Forgot your password?
+							</a>
+							<button>Sign In</button>
+						</form>
+					</div>
+					<div className="overlay-container" id="overlayCon">
+						<div className="overlay">
+							<div className="overlay-panel overlay-left">
+								<h1 className="h">Welcome Back!</h1>
+								<p>To keep connected with us please login with your personal info</p>
+								<button>Sign In</button>
+							</div>
+							<div className="overlay-panel overlay-right">
+								<h1 className="h">Hello, Friend!</h1>
+								<p>Enter your personal details and start journey with us</p>
+								<button>Sign Up</button>
+							</div>
+						</div>
+						<button id="overlayBtn"></button>
+					</div>
 				</div>
-			</div>
 			</section>
-
 
 			{/* <script>
 				const container=document.getelementbyid('container')
@@ -101,9 +99,6 @@ export default function Login() {
 
 			</script>
  */}
-
-
-			
 		</>
 	);
 }
