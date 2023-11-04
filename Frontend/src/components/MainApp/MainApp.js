5e880acd21d820ccb50f5f6f2848922fc6326c59
RiC8703 <99033832+RiC8703@users.noreply.github.com>
2023-11-04 12:43:29 +0530

    c

--- Frontend/src/components/MainApp/MainApp.js
+++ Frontend/src/components/MainApp/MainApp.js
@@ -2,26 +2,17 @@ import React from "react";
 import Discover from "./Discover/Discover";
 import Groups from "./Groups/Groups";
 import MainContent from "./Maincontent/MainContent";
-// import UserProfile from "./UserProfile/Profile";
-import Profile4 from "./Profile4";
 import "./Groups/groups.css";
 import "./Discover/discover.css";
 import "./Maincontent/maincontent.css";
-// import "./UserProfile/UserProfile.css";
-// import "./UserProfile/Profile3.css"
 
 
 const MainApp = () => {
 	return (
 		<div className="d-flex app-font absolute">
-			{/* <Groups /> */}
-			{/* <Discover /> */}
-			{/* <MainContent /> */}
-			{/* <UserProfile /> */}
-			<Profile4 />
-			
-
-
+			<Groups />
+			<Discover />
+			<MainContent />
 		</div>
 	);
 };
