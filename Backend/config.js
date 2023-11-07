ffee5074c9cf9d7b05d92ebaab159f3d031df31f
RiC8703 <99033832+RiC8703@users.noreply.github.com>
2023-11-07 19:35:52 +0530

    cinfig

--- Backend/config.js
+++ Backend/config.js
@@ -2,5 +2,5 @@ export default {
 	JWT_SECRET: "JZ9r5oBxM+7y0qOA396BucMbd8CHT/7PcPf4WQKrsvw=",
 	MONGODB_URL: "mongodb+srv://CodersPoint:%23Coder%23KRRD%24%24@coderspoint.tg5xpoe.mongodb.net/test",
 	EMAIL: "coderspoint.in@gmail.com",
-	PASSWORD: "qsfhirjjpdasnten",
+	PASSWORD: "wxpuaepvundbippj",
 };
