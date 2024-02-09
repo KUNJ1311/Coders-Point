4b6865b4c4c677ef9f1214cffbedf3be9c35f941
Kunj1311 <74526794+KUNJ1311@users.noreply.github.com>
2024-02-09 20:15:45 +0530

    fix

--- Frontend/src/components/HomePage/Home.js
+++ Frontend/src/components/HomePage/Home.js
@@ -4,8 +4,11 @@ import Typewriter from "typewriter-effect";
 import "./Home.css";
 import coder from "./g8.svg";
 import { useNavigate } from "react-router-dom";
-import home from "./banner-bg.png"
+import home from "./img/banner-bg.png";
 import PrinterAnimation from "./PrinterAnimation";
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter, FaInstagram, FaLinkedin } from "react-icons/fa6";
+
 const Home = () => {
 	const Navigate = useNavigate();
 
@@ -47,50 +50,75 @@ const Home = () => {
 							/>
 						</div>
 					</div>
-					<div className="coders-image">
-						{/* <img src={coder} width="400px" height="400px" alt="" /> */
-						<PrinterAnimation/>}
+					<div className="coders-image">{/* <img src={coder} width="400px" height="400px" alt="" /> */ <PrinterAnimation />}</div>
+				</div>
+				<div className="lorem">
+					Want to code together,Ajaoo
+					<br />
+					Want to chat while Coding, Ajaoo
+					<br />
+					Want to Share Screen while coding ,Ajaoo
+					<br />
+				</div>
+
+				<footer className="footer2">
+					<div className="waves">
+						<div className="wave" id="wave1"></div>
+						<div className="wave" id="wave2"></div>
+						<div className="wave" id="wave3"></div>
+						<div className="wave" id="wave4"></div>
 					</div>
-				</div>
-			<div className="lorem">
-				
-					Want to code together,Ajaoo<br/>
-					Want to chat while Coding, Ajaoo<br/>
-					Want to Share Screen while coding ,Ajaoo<br/>
-					
-			</div>
-			
-			<footer className="footer2">
-				<div className="waves">
-					<div className="wave" id="wave1"></div>
-					<div className="wave" id="wave2"></div>
-					<div className="wave" id="wave3"></div>
-					<div className="wave" id="wave4"></div>
-				</div>
-				<ul className="social-icon">
-					<li className="social-icon_item"><a className="social-icon_link" href="#">
-						<ion-icon name="logo-facebook"></ion-icon>
-					</a></li>
-					<li className="social-icon_item"><a className="social-icon_link" href="#">
-						<ion-icon name="logo-twitter" alt="twitter"></ion-icon>
-					</a></li>
-					<li className="social-icon_item"><a className="social-icon_link" href="#">
-						<ion-icon name="logo-linkedin"></ion-icon>
-					</a></li>
-					<li className="social-icon_item"><a className="social-icon_link" href="#">
-						<ion-icon name="logo-instagram"></ion-icon>
-					</a></li>
-				</ul>
-				<ul className="menu">
-					<pre><li className="menu_item"><a className="menu_link" href="#">Home</a></li>
-					<li className="menu_item"><a className="menu_link" href="#">About</a></li>
-					<li className="menu_item"><a className="menu_link" href="#">Services</a></li>
-					<li className="menu_item"><a className="menu_link" href="#">Team</a></li>
-					<li className="menu_item"><a className="menu_link" href="#">Contact</a></li>
-					</pre>
-				</ul>
-				<p>&copy;2023 CodersPoint | All Rights Reserved</p>
-			</footer>
+					<ul className="social-icon">
+						<li className="social-icon_item">
+							<a className="social-icon_link" href="https://www.facebook.com/">
+								<FaFacebook className="social-size" />
+							</a>
+						</li>
+						<li className="social-icon_item">
+							<a className="social-icon_link" href="https://twitter.com/">
+								<FaSquareXTwitter className="social-size" />
+							</a>
+						</li>
+						<li className="social-icon_item">
+							<a className="social-icon_link" href="https://www.instagram.com/">
+								<FaInstagram className="social-size" />
+							</a>
+						</li>
+						<li className="social-icon_item">
+							<a className="social-icon_link" href="https://www.linkedin.com/home">
+								<FaLinkedin className="social-size" />
+							</a>
+						</li>
+					</ul>
+					<ul className="menu">
+						<li className="menu_item">
+							<a className="menu_link" href="#">
+								Home
+							</a>
+						</li>
+						<li className="menu_item">
+							<a className="menu_link" href="#">
+								About
+							</a>
+						</li>
+						<li className="menu_item">
+							<a className="menu_link" href="#">
+								Services
+							</a>
+						</li>
+						<li className="menu_item">
+							<a className="menu_link" href="#">
+								Team
+							</a>
+						</li>
+						<li className="menu_item">
+							<a className="menu_link" href="#">
+								Contact
+							</a>
+						</li>
+					</ul>
+					<p>&copy;2023 CodersPoint | All Rights Reserved</p>
+				</footer>
 			</div>
 		</>
 	);
